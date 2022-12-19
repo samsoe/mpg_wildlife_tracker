9e0a035d152bbe39e9ebbad686d721315bc318b5
Erik Samsoe <esamsoe@gmail.com>
2022-12-19 12:51:55 -0700

    Create bigquery_access.py

---
+++ bigquery_access.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+from google.cloud import bigquery
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '../keys/credentials.json'
+project_id = 'mpg-wildlife-tracking'
+
+client = bigquery.Client(project=project_id)
+
+sql_query = """
+  SELECT *
+  FROM `mpg-wildlife-tracking.coyotes.main`
+  LIMIT 1000
+"""
+
+df = pd.read_gbq(sql_query, project_id=project_id)
+
+df.head()
